41f0b19de0fb08b78c907d4db5a3a830d37de009
clalil <cliljander@gmail.com>
2019-10-09 15:21:28 +0200

    Corrects bugged code, revert to previous.

--- app/services/cocktail_service.rb
+++ app/services/cocktail_service.rb
@@ -2,37 +2,24 @@ require 'rest-client'
 require 'json'
 
 module CocktailService
+
   def self.get_drinks(choice)
     unless choice == nil
-    choice_list = choice.downcase.split(', ')
-    id_list = []
-    available_cocktails = []
-
-      choice_list.each do |ingredient|
-      response = RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php", { params: { i: ingredient } } )
+      response = RestClient.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/filter.php",
+        {
+          params: {
+            i: choice
+          }
+        }
+      )
+      
       if response.body == ""
         'error'
-    else
+      else
         results = JSON.parse(response)
-          results['drinks'].each do |drink_name|
-            id_list.push drink_name['idDrink']
-          end
+        results['drinks']
       end
-
-      id_list.uniq.each do |id|
-      response = RestClient.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php", { params: { i: id } } )
-      results = JSON.parse(response)
-      ingredients = []
-        (1..15).each do |num|
-          gaur = results['drinks'][0]['strIngredient' + num.to_s]
-          ingredients.push(gaur.downcase) unless gaur.nil?      
-        end
-          if (ingredients & choice_list == ingredients)
-            available_cocktails.push( {id: id, drink: results['drinks'][0]} )
-          end
-        end
-        puts available_cocktails
-      end
-    end
+    end    
   end
 end
