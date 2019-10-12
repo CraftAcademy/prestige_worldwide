002cf17ef9352e0b55375f6d7b9e1f7a529b1ba5
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-12 17:46:16 +0200

    Undo changes in charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,6 +1,6 @@
 class ChargesController < ApplicationController
   def new
-    @membership = Membership.find(params[:user_id])
+    @membership = Membership.find(params[:id])
   end
 
   def create
