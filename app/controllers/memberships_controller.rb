9005164e7095b9f944dc90d00936888ec3606059
clalil <cliljander@gmail.com>
2019-10-13 10:30:33 +0200

    Correct typo.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,6 +1,6 @@
 class MembershipsController < ApplicationController
   def create
-    @membership = Memebership.new(membership_params)
+    @membership = Membership.new(membership_params)
     @membership.user_id = current_user.id
     @membership.save
     respond_with(@membership)
