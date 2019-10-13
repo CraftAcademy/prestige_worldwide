8caaf3bf02ca38de960abd7b16c5adba5653f865
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-13 15:56:49 +0200

    Refactor attemt to charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,27 +3,28 @@ class ChargesController < ApplicationController
     if params.has_key? :id
       @membership = Membership.find(params[:id])
     else
-      @membership = Membership.create
+      @membership = create
     end
   end
 
   def create
-    membership = Membership.find(params[:user_id])
-    binding.pry
+    
     user = Stripe::Customer.create(
       email: params[:email],
       source: get_token(params),#params['stripeToken'],
       description: "Member: #{params[:email]}"
     )
-    binding.pry
+    
     charge = Stripe::Charge.create(
       user: user.id,
       amount: 200 * 100,
       currency: 'usd',
-      description: "Purchase of #{membership.title}"
+      description: "Purchase of membership"
     )
-    binding.pry
+    
     if charge[:paid]
+      @membership = Membership.create
+
       redirect_to root_path, notice: "Well done, you are moving up in life!"
     else
       redirect_to root_path, notice: "I guess you'll always be a loser"
